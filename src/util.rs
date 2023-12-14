b4fd4506c868642fc3ef088128bdad27141513f3
yol <yol@casix.org>
2023-12-14 07:20:06 +0100

    fix parse_ints

--- src/util.rs
+++ src/util.rs
@@ -4,19 +4,20 @@ use std::{
     path::Path,
 };
 
+use std::fmt::Debug;
 use std::str::FromStr;
 
 use itertools::Itertools;
 
-/* pub fn parse_ints<T>(s: &str) -> Vec<T>
+pub fn parse_ints<T>(s: &str) -> Vec<T>
 where
     T: FromStr,
+    <T as FromStr>::Err: Debug,
 {
     s.split(|c| c == ',' || c == ' ')
         .map(|p| p.parse::<T>().unwrap())
-        .collect()
-        .into()
-} */
+        .collect_vec()
+}
 
 pub fn file_lines(filename: &str) -> Vec<String> {
     let file = File::open(Path::new(filename)).unwrap();
