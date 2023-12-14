c72a70f2049e0ec0ccca1c5c868d4ce390f57347
yol <yol@casix.org>
2023-12-14 07:10:20 +0100

    day14 improv

--- src/day14.rs
+++ src/day14.rs
@@ -77,23 +77,17 @@ pub fn part2() {
     let mut grid = Grid::from_vec(grid_vec, first_line.len());
 
     fn fall_north(grid: &mut Grid<u8>) {
-        for y in 0..grid.rows() {
+        for y in 1..grid.rows() {
             for x in 0..grid.cols() {
-                if grid[(y, x)] == ROUND {
-                    let mut move_by_y: Option<usize> = None;
-                    for y2 in 1..=y {
-                        match grid[(y - y2, x)] {
-                            EMPTY => {
-                                move_by_y = Some(y2);
-                            }
-                            ROUND | CUBE => break,
-                            _ => panic!(),
-                        }
-                    }
-                    if let Some(move_by_y) = move_by_y {
-                        grid[(y, x)] = EMPTY;
-                        grid[(y - move_by_y, x)] = ROUND;
-                    }
+                if grid[(y, x)] != ROUND {
+                    continue;
+                }
+                let move_to_y = num::range_step_inclusive((y - 1) as isize, 0, -1)
+                    .take_while(|&y2| grid[(y2 as usize, x)] == EMPTY)
+                    .last();
+                if let Some(move_to_y) = move_to_y {
+                    grid[(y, x)] = EMPTY;
+                    grid[(move_to_y as usize, x)] = ROUND;
                 }
             }
         }
@@ -115,11 +109,9 @@ pub fn part2() {
     }
     fn grid_hash(grid: &Grid<u8>) -> u64 {
         let mut hasher = DefaultHasher::new();
-        for ((y, x), &c) in grid.indexed_iter() {
-            if c == ROUND {
-                hasher.write_usize(y);
-                hasher.write_usize(x);
-            }
+        for ((y, x), _) in grid.indexed_iter().filter(|(_, &c)| c == ROUND) {
+            hasher.write_usize(y);
+            hasher.write_usize(x);
         }
         hasher.finish()
     }
