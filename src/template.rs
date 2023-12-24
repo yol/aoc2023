570f64a1873af2ed0aa11a675bbfc826a06e2fe4
yol <yol@casix.org>
2023-12-24 14:52:37 +0100

    template

---
+++ src/template.rs
@@ -0,0 +1,10 @@
+use crate::util::file_lines;
+use itertools::Itertools;
+
+pub fn part1() {
+    let lines = file_lines("inp18_1.txt");
+}
+
+pub fn part2() {
+    let lines = file_lines("inp18_1.txt");
+}
